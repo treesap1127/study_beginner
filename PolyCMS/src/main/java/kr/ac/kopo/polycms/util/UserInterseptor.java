8777c4f7c4aed84262ed019b28993b0802e3e021
treesap1127 <xotjddl1127@gmail.com>
2022-04-11 10:21:01 +0900

    test_study_commit

--- PolyCMS/src/main/java/kr/ac/kopo/polycms/util/UserInterseptor.java
+++ PolyCMS/src/main/java/kr/ac/kopo/polycms/util/UserInterseptor.java
@@ -1,8 +1,24 @@
 package kr.ac.kopo.polycms.util;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import kr.ac.kopo.polycms.model.Member;
+
 public class UserInterseptor extends HandlerInterceptorAdapter {
-	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		Member member =(Member) session.getAttribute("member");
+		if(member!=null) {
+			System.out.println("UserInterseptor : True");
+			return true;
+		}
+		return false;
+	}
 	
 }
